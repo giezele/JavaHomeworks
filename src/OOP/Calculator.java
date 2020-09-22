c0ff3dbab8304f9dd63119bd1130d164f3406b85
giezele <giedre.zel@gmail.com>
2020-09-22 23:45:19 +0300

    Static vs. Instance

--- src/OOP/Calculator.java
+++ src/OOP/Calculator.java
@@ -1,11 +1,21 @@
-package lt.giedre1.app;
+package OOP;
+
+// Difference between static and non-static method in Java
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println(StaticCalculator.add(4,5));
+        System.out.println("Static sudėtis: " + StaticCalculator.add(4,5));
+        System.out.println("Static atimtis: " + StaticCalculator.subtract(4,5));
+        System.out.println("Static dalyba: " + StaticCalculator.divide(4,5));
+        System.out.println("Static daugyba: " + StaticCalculator.multiply(4,5));
+
 // System.out.println((new NonstaticCalculator()).add(8, 9));
         NonstaticCalculator nsc = new NonstaticCalculator();
-        System.out.println(nsc.add(8, 9));
+        System.out.println("Instance sudėtis: " + nsc.add(8, 9));
+        System.out.println("Instance atimtis: " + nsc.subtract(8, 9));
+        System.out.println("Instance dalyba: " + nsc.divide(8, 9));
+        System.out.println("Instance daugyba: " + nsc.multiply(8, 9));
+
 
     }
 }
@@ -13,10 +23,34 @@ class StaticCalculator {
     public static int add(int i, int j){
         return i + j;
     }
+    public static int subtract(int i, int j){
+        return i - j;
+    }
+    public static int divide(int i, int j){
+        return i / j;
+    }
+    public static int multiply(int i, int j){
+        return i * j;
+    }
 }
 class NonstaticCalculator {
-     public int add(int i, int j){
-            return i + j;
-        }
+    public int add(int i, int j) {
+        return i + j;
     }
+
+    public int subtract(int i, int j) {
+        return i - j;
+    }
+
+    public int divide(int i, int j) {
+        if (j == 0) {
+            System.out.println("");
+        }
+        return i;
+    }
+
+    public int multiply(int i, int j) {
+        return i * j;
+    }
+}
 
